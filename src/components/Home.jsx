8858f176cdeb7a3692acfac1856342be83e6a3d3
Aniruddhaqre <aniruddhamestryqre@gmail.com>
2023-08-19 23:53:26 +0530

    first

---
+++ src/components/Home.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import ProductCard from "./ProductCard ";
+
+const Home = () => {
+  const productList = [
+    {
+      name: "Mac Book",
+      price: "12000",
+      imgSrc: "https://rukminim1.flixcart.com/image/850/1000/kruyw7k0/computer/v/x/v/na-thin-and-light-laptop-apple-original-imag5jt7u9fzenqb.jpeg?q=90",
+      id: "asdhoidsoahdioahdioadsdj",
+    },
+    {
+      name: "Shoes",
+      price: "490",
+      imgSrc: "https://www.jiomart.com/images/product/original/rvrgwpjvsp/bruton-trendy-sports-shoes-for-men-blue-product-images-rvrgwpjvsp-0-202209021256.jpg?im=Resize=(1000,1000)",
+      id: "asdhoidsfsdofhewufeeiowh",
+    },
+  ];
+
+  const addToCartHandler = (options) => {
+    console.log(options)
+  }
+
+  return (
+    <div className="home">
+      {productList.map((i) => (
+        <ProductCard
+          key={i.id}
+          imgSrc={i.imgSrc}
+          name={i.name}
+          price={i.price}
+          id={i.id}
+          handler={addToCartHandler}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Home;
